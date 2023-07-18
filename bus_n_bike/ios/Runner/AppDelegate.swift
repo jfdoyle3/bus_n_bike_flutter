4ac2af2d2d1d2109afd721bbf215fd66ca0e6d22
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-07-17 21:52:29 -0400

    Working on getting google maps to work, show map

--- bus_n_bike/ios/Runner/AppDelegate.swift
+++ bus_n_bike/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDQrS2oFDpMKaFKoyyfAqJl9urBBh_JD5Q")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
